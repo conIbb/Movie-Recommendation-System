0605837f6f11c2964476fb564ef515938d0594fb
conIbb <connoribbotson10@gmail.com>
2026-04-17 14:32:42 +0100

    Update main.py
    Full movie recommendation system in full.

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
 
 movieData = pd.read_csv("movies.csv")
-ratingsData = pd.read_csv("ratings.csv")
 
-print(movieData.head)
-print(ratingsData.head)
+movieData['genres'] = movieData['genres'].fillna('')
+
+tfidf = TfidfVectorizer(stop_words='english')
+
+tfidfMatrix = tfidf.fit_transform(movieData['genres'])
+
+cosineSim = linear_kernel(tfidfMatrix,tfidfMatrix)
+
+indices = pd.Series(movieData.index, index=movieData['title']).drop_duplicates()
+
+def getRecommendations(title, cosineSim=cosineSim):
+    idx = indices[title]
+
+    simScore = list(enumerate(cosineSim[idx]))
+
+    simScore = sorted(simScore, key=lambda x: x[1], reverse = True)
+
+    simScore = simScore[1:11]
+
+    movieIndices = [i[0] for i in simScore]
+    
+    return movieData['title'].iloc[movieIndices]
+print(getRecommendations('Toy Story (1995)'))
